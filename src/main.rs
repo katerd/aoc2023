68e66d271589ebfa30315c716fe5501953150889
Kate Reidy <kate.reidy.0@gmail.com>
2023-12-02 09:03:08 +0000

    day_2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
@@ -10,7 +11,85 @@ fn is_digit_str(contents: &String, i: usize, x: &str) -> bool {
 }
 
 fn main() {
-    day_1().ok();
+    //day_1().ok();
+    day_2().ok();
+}
+
+fn day_2() -> std::io::Result<()> {
+    let file_path = "2.txt";
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut result = 0;
+
+    let lines = contents.lines();
+
+    let max_cubes = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+
+    for game_line in lines {
+        //println!("{}", line);
+
+        let parts: Vec<&str> = game_line.split(":").collect();
+        let game_str = parts[0].replace("Game ", "");
+        let game_int = game_str.parse::<i32>().unwrap();
+
+        println!("Game: {}", game_int);
+
+        let draws: Vec<&str> = parts[1].split(";").collect();
+
+        let mut is_valid_game = true;
+
+        for draw in draws {
+
+            let mut draw_totals = HashMap::from([
+                ("red", 0),
+                ("green", 0),
+                ("blue", 0),
+            ]);
+
+            println!("  - Draw: {}", draw);
+
+            let groups: Vec<&str> = draw.split(",").collect();
+
+            for group in groups {
+                //println!("    - Group: {}", group);
+
+                let group_detail: Vec<&str> = group.trim().split(" ").collect();
+
+                let group_number = group_detail[0].parse::<i32>().unwrap();
+                let group_colour = group_detail[1];
+
+                println!("      - Color: {}, Number: {}", group_colour, group_number);
+
+                draw_totals.entry(group_colour).and_modify(|e| *e += group_number);
+            }
+
+            for game_total in draw_totals {
+                let max_cube_colour = max_cubes.get(&game_total.0).unwrap();
+                if &game_total.1 > max_cube_colour {
+                    is_valid_game = false;
+                    println!("    >>>>>>>>>>>>>> INVALID Game: {} - {} > {}", game_int, game_total.1, max_cube_colour);
+                }
+            }
+        }
+
+        if is_valid_game {
+            println!("    >>> VALID Game: {}", game_int);
+            result += game_int
+        }
+        else {
+            println!("    >>> INVALID Game: {}", game_int);
+        }
+    }
+
+    println!("Result: {}", result);
+
+    Ok(())
 }
 
 fn day_1() -> std::io::Result<()> {
