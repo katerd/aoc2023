4d16e8cebb90b1e6a7db721f4762152eb49837a2
Kate Reidy <kate.reidy.0@gmail.com>
2023-12-02 09:10:52 +0000

    day_2 part 2

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,66 @@ fn is_digit_str(contents: &String, i: usize, x: &str) -> bool {
 
 fn main() {
     //day_1().ok();
-    day_2().ok();
+    //day_2().ok();
+    day_2_part_2().ok();
+}
+
+fn day_2_part_2() -> std::io::Result<()> {
+    let file_path = "2.txt";
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut result = 0;
+
+    let lines = contents.lines();
+
+    for game_line in lines {
+        let mut game_totals = HashMap::from([
+            ("red", 0),
+            ("green", 0),
+            ("blue", 0),
+        ]);
+
+        let parts: Vec<&str> = game_line.split(":").collect();
+        let game_str = parts[0].replace("Game ", "");
+        let game_int = game_str.parse::<i32>().unwrap();
+
+        println!("Game: {}", game_int);
+
+        let draws: Vec<&str> = parts[1].split(";").collect();
+
+        for draw in draws {
+
+            println!("  - Draw: {}", draw);
+
+            let groups: Vec<&str> = draw.split(",").collect();
+
+            for group in groups {
+                let group_detail: Vec<&str> = group.trim().split(" ").collect();
+
+                let group_number = group_detail[0].parse::<i32>().unwrap();
+                let group_colour = group_detail[1];
+
+                println!("      - Color: {}, Number: {}", group_colour, group_number);
+
+                game_totals.entry(group_colour).and_modify(|e| *e = i32::max(*e, group_number));
+            }
+        }
+
+        let game_total =
+            game_totals.get("red").unwrap() *
+                game_totals.get("green").unwrap() *
+                game_totals.get("blue").unwrap();
+
+        result += game_total;
+
+        println!("    >>> Game: {}, Total: {}", game_int, game_total);
+    }
+
+    println!("Result: {}", result);
+
+    Ok(())
 }
 
 fn day_2() -> std::io::Result<()> {
